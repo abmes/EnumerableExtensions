2c8d0f85800e9800d0937d253f29c195b9894c6e
stefan <stef.tashev@gmail.com>
2016-01-21 22:55:15 +0200

    initial checkin

---
+++ Abmes.EnumerableExtensions/ElementWithContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abmes.EnumerableExtensions
+{
+    public class ElementWithContext<T>
+    {
+        public T Previous { get; private set; }
+        public T Next { get; private set; }
+        public T Current { get; private set; }
+        public int Index { get; private set; }
+        public bool IsFirst { get; private set; }
+        public bool IsLast { get; private set; }
+
+        public ElementWithContext(T current, T previous, T next, int index, bool isFirst, bool isLast)
+        {
+            Current = current;
+            Previous = previous;
+            Next = next;
+            Index = index;
+            IsFirst = isFirst;
+            IsLast = isLast;
+        }
+    }
+}
---
+++ Abmes.EnumerableExtensions/EnumerableExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abmes.EnumerableExtensions
+{
+    public static class EnumerableExtensions
+    {
+        private static IEnumerable<ElementWithContext<T>> GetEnumerableWithContext<T>(IEnumerable<T> source)
+        {
+            if (source.Any())
+            {
+                T previous = default(T);
+                T current = source.FirstOrDefault();
+                int index = 0;
+                bool isFirst = true;
+
+                foreach (T next in source.Skip(1))
+                {
+                    yield return new ElementWithContext<T>(current, previous, next, index, isFirst, false);
+                    previous = current;
+                    current = next;
+                    isFirst = false;
+                    index++;
+                }
+
+                yield return new ElementWithContext<T>(current, previous, default(T), index, false, true);
+            }
+        }
+
+        public static IEnumerable<ElementWithContext<T>> WithContext<T>(this List<T> source) => GetEnumerableWithContext(source);
+        public static IEnumerable<ElementWithContext<T>> WithContext<T>(this T[] source) => GetEnumerableWithContext(source);
+    }
+}
